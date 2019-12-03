74c3912255db90247dab0ef781e092322e60d73f
Felipe Valenzuela <felipevalenzuela@ug.uchile.cl>
2019-12-03 18:18:48 -0300

    send button problems fixed

--- src/animal_tornado/static/js/rostopic_listener.js
+++ src/animal_tornado/static/js/rostopic_listener.js
@@ -1,6 +1,12 @@
 // Connecting to ROS
 // -----------------
 function ws_connect(){
+var bg = document.getElementById("alpha")
+var button = document.getElementById("starto");
+button.style.display = "none";
+bg.style.display = "none";
+
+
 var ros = new ROSLIB.Ros({
   url : 'ws://localhost:9090'
 });
@@ -23,32 +29,6 @@ var animal_file = new ROSLIB.Topic({
   messageType : 'uchile_msgs/AnimalCard'
 });
 
-
-var animal_info = new ROSLIB.Topic({
-  ros : ros,
-  name : '/animal_info',
-  messageType : 'uchile_msgs/AnimalCard'
-});
-
-function info_publisher(){
-  console.log("Publishing!");
-  var nombre = document.getElementById("nombre");
-  var locacion = document.getElementById("locacion");
-  var restantes = document.getElementById("restantes");
-  var datos_extra = document.getElementById("datos_extra");
-  var autor = document.getElementById("autor");
-  var img = document.getElementById("foto");
-  var info = new ROSLIB.Message({
-    name : nombre.value,
-    location : locacion.value,
-    left_species : restantes.value,
-    pic : img.src,
-    extra_info : datos_extra.value,
-    author : autor.value
-  });
-  animal_info.publish(info);
-}
-
 // animal_info.publish("uwu","uwu","uwu","uwu","uwu","uwu");
 
 animal_file.subscribe(function(message) {
@@ -67,9 +47,46 @@ animal_file.subscribe(function(message) {
   var img = document.getElementById("foto");
   img.src = message.pic;
 });
-var bg = document.getElementById("alpha")
-var button = document.getElementById("starto");
-button.style.display = "none";
-bg.style.display = "none";
 
+}
+
+function info_publisher(){
+var ros = new ROSLIB.Ros({
+  url : 'ws://localhost:9090'
+});
+
+ros.on('connection', function() {
+  console.log('Connected to websocket server.');
+});
+
+ros.on('error', function(error) {
+  console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+  console.log('Connection to websocket server closed.');
+});
+  
+  var animal_info = new ROSLIB.Topic({
+    ros : ros,
+    name : '/animal_info',
+    messageType : 'uchile_msgs/AnimalCard'
+  });
+
+  console.log("Publishing!");
+  var nombre = document.getElementById("nombre");
+  var locacion = document.getElementById("locacion");
+  var restantes = document.getElementById("restantes");
+  var datos_extra = document.getElementById("datos_extra");
+  var autor = document.getElementById("autor");
+  var img = document.getElementById("foto");
+  var info = new ROSLIB.Message({
+    name : nombre.value,
+    location : locacion.value,
+    left_species : restantes.value,
+    pic : img.src,
+    extra_info : datos_extra.value,
+    author : autor.value
+  });
+  animal_info.publish(info);
 }
